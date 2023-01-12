6f47665a36ee016d8e83910569e184fe0a929cfc
Brooke Mitchell <927600+brookemitchell@users.noreply.github.com>
2023-01-12 23:01:01 +1300

    toggle form colapse/show with js

--- lib/pento_p2_web/live/demographic_live/show.ex
+++ lib/pento_p2_web/live/demographic_live/show.ex
@@ -1,15 +1,36 @@
 defmodule PentoP2Web.DemographicLive.Show do
   use Phoenix.{Component, HTML}
-  alias PentoP2Web.DemographicLive
+  alias Phoenix.LiveView.JS
+
+  def toggle_modal(js \\ %JS{}) do
+    js
+    |> JS.toggle(to: "#demo-section")
+    |> JS.remove_class(
+      "expanded",
+      to: "#button.expanded"
+    )
+    |> JS.add_class(
+      "expanded",
+      to: "#button:not(.expanded)"
+    )
+  end
 
   def details(assigns) do
     ~H"""
       <div class="survey-component-container">
         <h2>Demographics: <%= raw "&#x2713" %></h2>
-        <ul>
-          <li>Gender <%= @demographic.gender %></li>
-          <li>Year of birth: <%= @demographic.year_of_birth %></li>
-        </ul>
+        <button
+          class="button button-outline"
+          id="button"
+          phx-value="show"
+          phx-click={toggle_modal()}
+          />
+        <div id="demo-section" class="" >
+          <ul>
+            <li>Gender <%= @demographic.gender %></li>
+            <li>Year of birth: <%= @demographic.year_of_birth %></li>
+          </ul>
+        </div>
       </div>
     """
   end

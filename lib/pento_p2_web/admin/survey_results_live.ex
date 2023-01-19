2fd31c314a2f7929a8b546a295f69682a52810e4
Brooke Mitchell <927600+brookemitchell@users.noreply.github.com>
2023-01-20 12:09:49 +1300

    start chart plot and filter work

--- lib/pento_p2_web/admin/survey_results_live.ex
+++ lib/pento_p2_web/admin/survey_results_live.ex
@@ -2,31 +2,67 @@ defmodule PentoP2Web.Admin.SurveyResultsLive do
   use PentoP2Web, :live_component
 
   alias PentoP2.Catalog
+  alias Contex.Plot
 
   def update(assigns, socket) do
     {:ok,
      socket
      |> assign(assigns)
-     |> assign_products_with_average_ratings()}
+     |> assign_products_with_average_ratings()
+     |> assign_dataset()
+     |> assign_chart()
+     |> assign_chart_svg()}
   end
 
-  defp assign_products_with_average_ratings(socket) do
+  def assign_chart(%{assigns: %{dataset: dataset}} = socket) do
+    socket
+    |> assign(:chart, make_bar_chart(dataset))
+  end
+
+  def assign_chart_svg(%{assigns: %{chart: chart}} = socket) do
+    socket
+    |> assign(:chart_svg, render_bar_chart(chart))
+  end
+
+  def assign_dataset(
+        %{
+          assigns: %{
+            products_with_average_ratings: products_with_average_ratings
+          }
+        } = socket
+      ) do
     socket
     |> assign(
-      :products_with_average_ratings,
-      Catalog.products_with_average_ratings()
+      :dataset,
+      make_bar_chart_dataset(products_with_average_ratings)
     )
   end
 
-  def render(assigns) do
-    ~H"""
-      <div>
-        <ul>
-        <%= for  {name, rating} <- @products_with_average_ratings do %>
-          <li><%= name %> - <%= rating %></li>
-        <% end %>
-        </ul>
-      </div>
-    """
+  def assign_products_with_average_ratings(socket) do
+    socket
+    # |> assign(
+    #   :products_with_average_ratings,
+    #   Catalog.products_with_average_ratings()
+    # )
   end
+
+  defp make_bar_chart_dataset(data) do
+    Contex.Dataset.new(data)
+  end
+
+  defp make_bar_chart(dataset) do
+    Contex.BarChart.new(dataset)
+  end
+
+  defp render_bar_chart(chart) do
+    Plot.new(500, 400, chart)
+    |> Plot.titles(title(), subtitle())
+    |> Plot.axis_labels(x_axis(), y_axis())
+    |> Plot.to_svg()
+  end
+
+  defp title, do: "Title ratings"
+  defp subtitle, do: "average star ratings per product"
+  defp x_axis, do: "products"
+  defp y_axis, do: "stars"
 end
--- mix.exs
+++ mix.exs
@@ -34,22 +34,23 @@ defmodule PentoP2.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.6.15"},
-      {:phoenix_ecto, "~> 4.4"},
+      {:contex, "~> 0.4.0"},
       {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.6"},
       {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
+      {:floki, ">= 0.30.0", only: :test},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:phoenix_ecto, "~> 4.4"},
+      {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_dashboard, "~> 0.6"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 0.17.5"},
+      {:phoenix, "~> 1.6.15"},
+      {:plug_cowboy, "~> 2.5"},
+      {:postgrex, ">= 0.0.0"},
+      {:swoosh, "~> 1.3"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
